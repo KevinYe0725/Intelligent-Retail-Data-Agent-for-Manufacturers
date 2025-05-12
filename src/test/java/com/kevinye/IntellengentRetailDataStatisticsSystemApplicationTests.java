5a287712c71cbd5227fbbef8a434a7bf33a61404
KevinYe0725 <kevinye543@gmail.com>
2025-05-12 09:24:15 +0800

    初始项目

---
+++ src/test/java/com/kevinye/IntellengentRetailDataStatisticsSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kevinye;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntellengentRetailDataStatisticsSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
