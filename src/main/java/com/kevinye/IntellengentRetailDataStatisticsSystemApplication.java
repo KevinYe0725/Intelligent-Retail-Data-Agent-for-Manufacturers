886b5f5ff59456153167d4a9b7fc33d082518747
kevinye <15514171+kevin_ye_01@user.noreply.gitee.com>
2025-05-12 09:24:15 +0800

    初始项目

---
+++ src/main/java/com/kevinye/IntellengentRetailDataStatisticsSystemApplication.java
@@ -0,0 +1,13 @@
+package com.kevinye;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntellengentRetailDataStatisticsSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntellengentRetailDataStatisticsSystemApplication.class, args);
+    }
+
+}
