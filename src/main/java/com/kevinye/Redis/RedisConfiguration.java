611f13b97c220579d0e46fed40c417f64c5b9fe1
KevinYe0725 <kevinye543@gmail.com>
2025-05-20 21:32:27 +0800

    初始项目

---
+++ src/main/java/com/kevinye/Redis/RedisConfiguration.java
@@ -0,0 +1,32 @@
+package com.kevinye.Redis;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Slf4j
+@Configuration
+public class RedisConfiguration {
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate redisTemplate = new RedisTemplate();
+
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        //序列化器
+        GenericJackson2JsonRedisSerializer genericJackson2JsonRedisSerializer = new GenericJackson2JsonRedisSerializer();
+
+        //设置Value序列化器（因为值的类型并不确定，所以应该要是generic的多态序列化器）
+        redisTemplate.setValueSerializer(genericJackson2JsonRedisSerializer);
+        //String的序列化器
+        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+        redisTemplate.setKeySerializer(stringRedisSerializer);
+        redisTemplate.setHashKeySerializer(stringRedisSerializer);
+        redisTemplate.setHashValueSerializer(genericJackson2JsonRedisSerializer);
+
+        return redisTemplate;
+    }
+}
